adb89147df2e7cdcef33c5ff897038d46f0a2a5e
Joel A. Charbonnet Jr. <CharboLinux@gmail.com>
2026-02-26 13:32:52 -0800

    Initial commit of Ep002 and Ep005

---
+++ Ep005/src/main.cpp
@@ -0,0 +1,15 @@
+/*
+    Source Code For This CMake Series:
+    https://github.com/rutura/CMakeSeries
+ */
+#include <iostream>
+#include "dog.h"
+#include "operations.h"
+
+int main()
+{
+    Dog dog1("Flitzy");
+    dog1.print_info();
+
+    return 0;
+}
